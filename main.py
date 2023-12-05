d133c5117958adfac0aa775751b7d6c9ea9091b1
Gmarcum0641 <151578492+Gmarcum0641@users.noreply.github.com>
2023-12-05 13:14:24 -0500

    Gui Update

--- main.py
+++ main.py
@@ -3,20 +3,29 @@ import random
 import tkinter
 from tkinter import *
 from tkinter import filedialog
-from PIL import Image, ImageTk
 from itertools import count, cycle
 import numpy as np
 
 root = Tk()
 
+def getFilename():
+    print("hi")
 
 def UploadAction(event=None):
   global filename
-  filename = filedialog.askopenfilename()
-  print('Selected:', filename)
-  labelText = filename
+  filedir = filedialog.askopenfilename()
+  print('Selected:', filedir)
+  filename = os.path.basename(filedir).split('/')[-1]
   fileNameLabel.config(text=filename)
-
+  root.update()
+  root.geometry('%dx%d+%d+%d' % (
+      max(
+        importButton.winfo_width() + fileNameLabel.winfo_width() + 4,
+        fileInfoLabel.winfo_width() + 4
+      ),
+      50,
+      0,
+      0))
 
 importButton = Button(root, text='Open', command=UploadAction)
 importButton.place(x=0, y=0)
@@ -32,5 +41,15 @@ fileNameLabel.place(
     x=importButton.winfo_width() + 2,
     y=(importButton.winfo_height() - fileNameLabel.winfo_height()) / 2)
 
-root.geometry('%dx%d+%d+%d' % (100, 100, 0, 0))
+time = 0
+highAmp = 0
+rt60Dif = 0
+
+fileInfoLabel = Label(root, text="Time: " + str(time) + "   ; High Amp: " + str(highAmp) + "    ; RT60 Dif: " + str(rt60Dif))
+fileInfoLabel.place(x=2,
+    y=importButton.winfo_height())
+
+root.update()
+
+root.geometry('%dx%d+%d+%d' % (fileInfoLabel.winfo_width() + 4, 50, 0, 0))
 root.mainloop()
