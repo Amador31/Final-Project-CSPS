1491217a63f1a0c896e894994ee3cd4284ef4cca
Nicovenant <nicolasegaldona@gmail.com>
2023-12-08 14:35:59 -0500

    Changed to mono conversion instead of stereo conversion

--- clean.py
+++ clean.py
@@ -7,12 +7,20 @@ def clean_audio_data(file_path):
     def convert_to_wav(input_file, output_file):
         audio = AudioSegment.from_file(input_file)
 
-        # Ensure the output file has a .wav extension
-        if not output_file.lower().endswith('.wav'):
-            output_file = os.path.splitext(output_file)[0] + '.wav'
-
         # Export the audio as a WAV file
         audio.export(output_file, format='wav')
+        print(f"Conversion successful. Saved as {output_file}")
+
+        return output_file
+
+    def convert_to_mono(input_file, output_file):
+        audio = AudioSegment.from_file(file_path)
+
+        # Set channels to 1
+        mono_audio = audio.set_channels(1)
+
+        # Export the audio as a WAV file
+        mono_audio.export(output_file, format='wav')
         print(f"Conversion successful. Saved as {output_file}")
 
         return output_file
@@ -24,25 +32,23 @@ def clean_audio_data(file_path):
     # Extract only samplerate and data
     samplerate, data = wavfile.read(file_path)
 
+    # Check if channel format is more than 1, convert to mono
+    if data.ndim > 1:
+        file_path = convert_to_mono(file_path, 'mono_audio.wav')
+        # Update with new sample rate and data values for mono
+        samplerate, data = wavfile.read(file_path)
+
     # Handles missing values
     data = np.nan_to_num(data)
 
-    # Check if channel format is mono, convert to stereo
-    if data.ndim == 1:
-        # Convert to stereo by duplicating the mono channel
-        data = np.column_stack((data, data))
-    # Check if channel format is more than 2, convert to stereo
-    elif data.shape[1] > 2:
-        # Take the first two channels from multichannel recording
-        data = data[:, :2]
-
     # Create file path for new data
     cleaned_file_path = 'cleaned_audio.wav'
+
     # Rewriting file ensures no meta data
     wavfile.write(cleaned_file_path, samplerate, data)
     print(f"Cleaning successful. Saved as {cleaned_file_path}")
 
     return cleaned_file_path
 
-input_file_path = 'clap.mp3'
+input_file_path = '16bit4chan.wav'
 cleaned_file_path = clean_audio_data(input_file_path)
