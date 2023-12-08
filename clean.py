1a8f55d36e3b8c331816732da1a6352b27cdbc0d
Nicovenant <nicolasegaldona@gmail.com>
2023-12-08 12:22:59 -0500

    Finalizing cleaning code

--- clean.py
+++ clean.py
@@ -3,46 +3,42 @@ import numpy as np
 from pydub import AudioSegment
 import os
 
-def convert_to_wav(input_file, output_file):
-    audio = AudioSegment.from_file(input_file)
-
-    # Ensure the output file has a .wav extension
-    if not output_file.lower().endswith('.wav'):
-        output_file = os.path.splitext(output_file)[0] + '.wav'
-
-    # Export the audio as a WAV file
-    audio.export(output_file, format='wav')
-    print(f"Conversion successful. Saved as {output_file}")
-
-    return output_file
-
 def clean_audio_data(file_path):
+    def convert_to_wav(input_file, output_file):
+        audio = AudioSegment.from_file(input_file)
+
+        # Ensure the output file has a .wav extension
+        if not output_file.lower().endswith('.wav'):
+            output_file = os.path.splitext(output_file)[0] + '.wav'
+
+        # Export the audio as a WAV file
+        audio.export(output_file, format='wav')
+        print(f"Conversion successful. Saved as {output_file}")
+
+        return output_file
+
+    # Convert to WAV file if not already WAV
     if not file_path.lower().endswith('.wav'):
         file_path = convert_to_wav(file_path, 'converted_audio.wav')
 
+    # Extract only samplerate and data
     samplerate, data = wavfile.read(file_path)
 
-    # Handle missing values (replace NaN or Inf with zeros)
+    # Handles missing values
     data = np.nan_to_num(data)
 
-    # Check and handle inconsistencies in channel format
+    # Check if channel format is mono, convert to stereo
     if data.ndim == 1:
-        # If the data is mono, convert it to stereo by duplicating the channel
+        # Convert to stereo by duplicating the mono channel
         data = np.column_stack((data, data))
-        channels = 2
+    # Check if channel format is more than 2, convert to stereo
     elif data.shape[1] > 2:
-        # If there are more than 2 channels, take the first two channels
+        # Take the first two channels from multichannel recording
         data = data[:, :2]
-        channels = data.shape[1]
 
-    # Remove metadata (if any)
-    # You can use the pydub library to create a new AudioSegment without metadata
-    audio = AudioSegment(data.tobytes(), frame_rate=samplerate, sample_width=data.dtype.itemsize, channels=channels)
-
-    # Now 'data' contains the cleaned audio data
-
-    # Optionally, you can save the cleaned data to a new WAV file
+    # Create file path for new data
     cleaned_file_path = 'cleaned_audio.wav'
+    # Rewriting file ensures no meta data
     wavfile.write(cleaned_file_path, samplerate, data)
 
     return cleaned_file_path
